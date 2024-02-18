0b6740906d10467d38e623fa07189fd5dae2bd12
Fabiotriana07 <fabiotrianaar1707@gmail.com>
2024-02-17 21:06:49 -0500

    Actualizacion archivos

--- src/Scrabble.h
+++ src/Scrabble.h
@@ -1,67 +1,27 @@
-#include <iostream>
-#include "Scrabble.h"
+#ifndef SCRABBLE_H
+#define SCRABBLE_H
 
-using namespace std;
+#include <string>
 
-int main() {
-    Scrabble scrabble;
-    string comando;
-    
-    cout << "Bienvenido al juego Scrabble!" << endl;
-    cout << "Ingrese un comando ('ayuda' para ver la lista de comandos):" << endl;
-    
-    while (true) {
-        cout << "$ ";
-        cin >> comando;
+class Scrabble {
+public:
+    // Métodos públicos de la clase Scrabble
+    void inicializarDiccionario(const std::string& archivoDiccionario);
+    void iniciarDiccionarioInverso(const std::string& archivoDiccionario);
+    void obtenerPuntaje(const std::string& palabra);
+    void salir();
+    void iniciarArbol(const std::string& archivoDiccionario);
+    void iniciarArbolInverso(const std::string& archivoDiccionario);
+    void palabrasPorPrefijo(const std::string& prefijo);
+    void palabrasPorSufijo(const std::string& sufijo);
+    void grafoDePalabras();
+    void posiblesPalabras(const std::string& letras);
 
-        if (comando == "ayuda") {
-            // Mostrar lista de comandos disponibles
-            cout << "Lista de comandos:" << endl;
-            cout << "  - inicializar <archivoDiccionario>: Inicializa el diccionario con el archivo especificado." << endl;
-            cout << "  - iniciar_inverso <archivoDiccionario>: Inicializa el diccionario inverso con el archivo especificado." << endl;
-            cout << "  - puntaje <palabra>: Obtiene el puntaje de la palabra dada." << endl;
-            cout << "  - salir: Termina la ejecución del programa." << endl;
-            cout << "  - iniciar_arbol <archivoDiccionario>: Inicializa el árbol de palabras con el archivo especificado." << endl;
-            cout << "  - iniciar_arbol_inverso <archivoDiccionario>: Inicializa el árbol de palabras inverso con el archivo especificado." << endl;
-            cout << "  - palabras_por_prefijo <prefijo>: Muestra las palabras que comienzan con el prefijo dado." << endl;
-            cout << "  - palabras_por_sufijo <sufijo>: Muestra las palabras que terminan con el sufijo dado." << endl;
-        } else if (comando == "inicializar") {
-            string archivoDiccionario;
-            cin >> archivoDiccionario;
+private:
+    // Declaraciones privadas de la clase Scrabble
+    // Puedes agregar variables miembro aquí si es necesario
+};
 
-            scrabble.inicializarDiccionario(archivoDiccionario);
-        } else if (comando == "iniciar_inverso") {
-            string archivoDiccionario;
-            cin >> archivoDiccionario;
-            scrabble.iniciarDiccionarioInverso(archivoDiccionario);
-        } else if (comando == "puntaje") {
-            string palabra;
-            cin >> palabra;
-            scrabble.obtenerPuntaje(palabra);
-        } else if (comando == "salir") {
-            cout << "¡Have a nice day!" << endl;
-            break;
-        } else if (comando == "iniciar_arbol") {
-            string archivoDiccionario;
-            cin >> archivoDiccionario;
-            scrabble.iniciarArbol(archivoDiccionario);
-        } else if (comando == "iniciar_arbol_inverso") {
-            string archivoDiccionario;
-            cin >> archivoDiccionario;
-            scrabble.iniciarArbolInverso(archivoDiccionario);
-        } else if (comando == "palabras_por_prefijo") {
-            string prefijo;
-            cin >> prefijo;
-            scrabble.palabrasPorPrefijo(prefijo);
-        } else if (comando == "palabras_por_sufijo") {
-            string sufijo;
-            cin >> sufijo;
-            scrabble.palabrasPorSufijo(sufijo);
-        } else {
-            cout << "Comando no reconocido. Ingrese 'ayuda' para ver la lista de comandos disponibles." << endl;
-        }
-    }
+#endif // SCRABBLE_H
 
-    return 0;  
-}
 
